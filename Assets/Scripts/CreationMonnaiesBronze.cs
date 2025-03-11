455f6769febdbd32cdfa64cc6888e02f348411a7
B-Vincent01 <vin100binette@gmail.com>
2025-03-11 14:54:01 -0400

    fin cours

--- Assets/Scripts/CreationMonnaiesBronze.cs
+++ Assets/Scripts/CreationMonnaiesBronze.cs
@@ -6,11 +6,11 @@ public class CreationMonnaiesBronze : MonoBehaviour
 {
 
     [SerializeField] GameObject _prefabOriginal;
-    [SerializeField] Vector3 _positionCreation;
 
     void Start()
     {
-        Quaternion _rotation = Quaternion.Euler(0f, 90f, 0f);
-        Instantiate(_prefabOriginal, _positionCreation, _rotation);
+        Vector3 _positionActuel = transform.position;
+        Quaternion _rotation = transform.rotation;
+        Instantiate(_prefabOriginal, _positionActuel, _rotation);
     }
 }
---
+++ Assets/Scripts/PositionChampignon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionChampignon : MonoBehaviour
+{
+
+    [SerializeField] private float _positionChampignon;
+
+    void Start()
+    {
+
+        float randomPosition = Random.value;
+
+        if(randomPosition <= 0.5f)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
